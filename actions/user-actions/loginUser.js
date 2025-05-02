ca49ceb0733836fcd863228de1c94f8fc220ce52
Gopal <raunakchd1@gmail.com>
2025-05-03 01:04:42 +0530

    updated cors and http syntax for prod

--- actions/user-actions/loginUser.js
+++ actions/user-actions/loginUser.js
@@ -41,7 +41,7 @@ const loginUser = async (req, res) => {
 
     res.cookie(COOKIE_NAME, token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
